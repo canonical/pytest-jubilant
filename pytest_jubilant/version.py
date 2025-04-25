ed12322e5144393f13833add78b1bb36ac03e99f
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-04-25 10:01:49 +0200

    dynamic version

---
+++ pytest_jubilant/version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Version meta.
+Overridden by CI; do not modify.
+"""
+
+__version__ = "<overridden by CI; do not modify>"
