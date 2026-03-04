1ddb9bb27b8ad08ee5bc027207588c72abb3cb08
James Garner <james.garner@canonical.com>
2026-03-05 12:59:24 +1300

    chore: make patching function name clearer

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,13 +13,13 @@ pytest_jubilant.main._TESTING_RANDBITS_OVERRIDE = "testing"
 
 
 # 2) mock subprocess.run globally for all tests
-def mock_cli():
+def _patch_subprocess_run():
     mm = unittest.mock.MagicMock()
     mm.return_value = unittest.mock.MagicMock(stdout="output", stderr="error")
     return unittest.mock.patch("subprocess.run", new=mm)
 
 
-mock_cli().start()
+_patch_subprocess_run().start()
 
 
 # 3) Re-mock at module scope so that we can inspect the calls in the tests.
@@ -27,5 +27,5 @@ mock_cli().start()
 # Tests must request cli_mock first (leftmost) so it runs before the fixtures under test.
 @pytest.fixture(scope="module")
 def cli_mock():
-    with mock_cli() as mm:
+    with _patch_subprocess_run() as mm:
         yield mm
