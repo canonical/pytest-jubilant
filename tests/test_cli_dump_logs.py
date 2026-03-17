a89b3a645a4f217acda27e15d4f954c8d869378b
James Garner <james.garner@canonical.com>
2026-03-17 13:10:48 +1300

    test: better test file name

--- tests/test_cli_dump_logs.py
+++ tests/test_cli_dump_logs.py
@@ -3,7 +3,7 @@ from pathlib import Path
 pytest_plugins = ["pytester"]
 
 CONFTEST = (Path(__file__).parent / "conftest.py").read_text()
-TEST = """
+TEST_FILE = """
 def test_use_factory(temp_model_factory):
     temp_model_factory.get_juju("foo")
     temp_model_factory.get_juju("bar")
@@ -12,7 +12,7 @@ def test_use_factory(temp_model_factory):
 
 def test_dump_logs_not_passed(pytester):
     pytester.makeconftest(CONFTEST)
-    pytester.makepyfile(test_sample=TEST)
+    pytester.makepyfile(test_file=TEST_FILE)
 
     result = pytester.runpytest()
     result.assert_outcomes(passed=1)
@@ -22,30 +22,30 @@ def test_dump_logs_not_passed(pytester):
 
 def test_dump_logs_default_path(pytester):
     pytester.makeconftest(CONFTEST)
-    pytester.makepyfile(test_sample=TEST)
+    pytester.makepyfile(test_file=TEST_FILE)
 
     result = pytester.runpytest("--dump-logs")
     result.assert_outcomes(passed=1)
 
-    foo_log_path = pytester.path / ".logs" / "test-sample-testing-foo-jdl.txt"
+    foo_log_path = pytester.path / ".logs" / "test-file-testing-foo-jdl.txt"
     assert foo_log_path.exists()
     assert foo_log_path.read_text() == "stdout patched by conftest.py"
-    bar_log_path = pytester.path / ".logs" / "test-sample-testing-bar-jdl.txt"
+    bar_log_path = pytester.path / ".logs" / "test-file-testing-bar-jdl.txt"
     assert bar_log_path.exists()
     assert bar_log_path.read_text() == "stdout patched by conftest.py"
 
 
 def test_dump_logs_custom_path(pytester, tmp_path):
     pytester.makeconftest(CONFTEST)
-    pytester.makepyfile(test_sample=TEST)
+    pytester.makepyfile(test_file=TEST_FILE)
     custom_dir = tmp_path / "custom-logs"
 
     result = pytester.runpytest("--dump-logs", str(custom_dir))
     result.assert_outcomes(passed=1)
 
-    foo_log_path = custom_dir / "test-sample-testing-foo-jdl.txt"
+    foo_log_path = custom_dir / "test-file-testing-foo-jdl.txt"
     assert foo_log_path.exists()
     assert foo_log_path.read_text() == "stdout patched by conftest.py"
-    bar_log_path = custom_dir / "test-sample-testing-bar-jdl.txt"
+    bar_log_path = custom_dir / "test-file-testing-bar-jdl.txt"
     assert bar_log_path.exists()
     assert bar_log_path.read_text() == "stdout patched by conftest.py"
