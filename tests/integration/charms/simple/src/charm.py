bd9132e7b7fa7733194917f4e0bad54b7399219a
James Garner <james.garner@canonical.com>
2026-03-19 00:52:21 +1300

    test: correct event name

--- tests/integration/charms/simple/src/charm.py
+++ tests/integration/charms/simple/src/charm.py
@@ -10,7 +10,7 @@ import ops
 class SimpleCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        framework.observe(self.on.collect_status, self._on_collect_status)
+        framework.observe(self.on.collect_unit_status, self._on_collect_status)
 
     def _on_collect_status(self, event: ops.CollectStatusEvent):
         event.add_status(ops.ActiveStatus())
