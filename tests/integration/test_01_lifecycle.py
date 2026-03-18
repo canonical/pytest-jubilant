7ccf50040e67251639909e45435834a81a5ad78a
James Garner <james.garner@canonical.com>
2026-03-19 01:39:21 +1300

    test: fix assertions for real

--- tests/integration/test_01_lifecycle.py
+++ tests/integration/test_01_lifecycle.py
@@ -2,29 +2,35 @@ import pathlib
 
 pytest_plugins = ["pytester"]
 
-TEST_FILE = (pathlib.Path(__file__).parent / "lifecycle_tests.py").read_text()
+TEST_PASS = (pathlib.Path(__file__).parent / "lifecycle_tests.py").read_text()
 
 
-def test_dump_logs_not_passed(pytester):
-    pytester.makepyfile(test_file=TEST_FILE)
+def test_no_dump_logs(pytester):
+    pytester.makepyfile(test_file=TEST_PASS)
 
     result = pytester.runpytest()
 
     outcomes = result.parseoutcomes()
     print(outcomes)
-    result.assert_outcomes(failed=0, errors=0, warnings=0)
+    assert outcomes.get('passed')
+    assert not outcomes.get('failed')
+    assert not outcomes.get('errors')
+    assert not outcomes.get('warnings')
 
     assert not (pytester.path / ".logs").exists()
 
 
 def test_dump_logs_default_path(pytester):
-    pytester.makepyfile(test_file=TEST_FILE)
+    pytester.makepyfile(test_file=TEST_PASS)
 
     result = pytester.runpytest("--model", "modelt", "--dump-logs")
 
     outcomes = result.parseoutcomes()
     print(outcomes)
-    result.assert_outcomes(failed=0, errors=0, warnings=0)
+    assert outcomes.get('passed')
+    assert not outcomes.get('failed')
+    assert not outcomes.get('errors')
+    assert not outcomes.get('warnings')
 
     foo_log_path = pytester.path / ".logs" / "modelt-foo-jdl.txt"
     assert foo_log_path.exists()
@@ -33,14 +39,17 @@ def test_dump_logs_default_path(pytester):
 
 
 def test_dump_logs_custom_path(pytester, tmp_path):
-    pytester.makepyfile(test_file=TEST_FILE)
+    pytester.makepyfile(test_file=TEST_PASS)
     custom_dir = tmp_path / "custom-logs"
 
     result = pytester.runpytest("--model", "modelt", "--dump-logs", str(custom_dir))
 
     outcomes = result.parseoutcomes()
     print(outcomes)
-    result.assert_outcomes(failed=0, errors=0, warnings=0)
+    assert outcomes.get('passed')
+    assert not outcomes.get('failed')
+    assert not outcomes.get('errors')
+    assert not outcomes.get('warnings')
 
     foo_log_path = custom_dir / ".logs" / "modelt-foo-jdl.txt"
     assert foo_log_path.exists()
