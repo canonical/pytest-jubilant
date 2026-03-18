efc697fa825c5f98482bb4c0f4a04ca4e5a844c0
James Garner <james.garner@canonical.com>
2026-03-19 10:02:23 +1300

    chore: format

--- tests/integration/test_01_dump_logs.py
+++ tests/integration/test_01_dump_logs.py
@@ -81,13 +81,25 @@ def test_juju_debug_log_on_failure(pytester, tmp_path):
     assert bar_log_path.exists()
 
     # We emit the last 1000 lines of ``juju debug-log`` for each model if tests fail.
+    # Model 'model-t-foo':
     foo_msg = "*Logging last 1000 lines of ``juju debug-log`` for model model-t-foo:"
-    foo_lines = result.stderr.get_lines_after("*{foo_msg}")  # Match with fnmatch
+    foo_lines = result.stderr.get_lines_after(f"*{foo_msg}")  # Match with fnmatch
     for i, line in enumerate(foo_lines):
         if "Wrote full ``juju debug-log`` for model" in line:
-            end = i
+            foo_end = i
             break
     else:  # no break
         print(foo_lines)
         assert False, "Didn't find expected message about writing the full log!"
-    assert end == 1000
+    assert foo_end == 1000
+    # Model 'model-t-bar':
+    bar_msg = "*Logging last 1000 lines of ``juju debug-log`` for model model-t-bar:"
+    bar_lines = result.stderr.get_lines_after(f"*{bar_msg}")  # Match with fnmatch
+    for i, line in enumerate(bar_lines):
+        if "Wrote full ``juju debug-log`` for model" in line:
+            bar_end = i
+            break
+    else:  # no break
+        print(bar_lines)
+        assert False, "Didn't find expected message about writing the full log!"
+    assert bar_end == 1000
