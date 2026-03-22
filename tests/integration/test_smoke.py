d721c2b37beea5dda0bb96589a7d3921911584b7
James Garner <james.garner@canonical.com>
2026-03-23 12:15:43 +1300

    test: use an even higher timeout I guess

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -5,4 +5,4 @@ import jubilant
 
 def test_smoke(juju: jubilant.Juju, simple_charm: pathlib.Path):
     juju.deploy(simple_charm)
-    juju.wait(jubilant.all_active, timeout=600)
+    juju.wait(jubilant.all_active, timeout=60 * 30)
