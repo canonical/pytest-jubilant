7e1891d4a761cc5470fa48faea6d2f2c796995d0
James Garner <james.garner@canonical.com>
2026-03-24 12:47:22 +1300

    test: ensure that we get all the logs on success (when we don't wait)

--- tests/integration/test_dump_logs.py
+++ tests/integration/test_dump_logs.py
@@ -15,8 +15,8 @@ if typing.TYPE_CHECKING:
 pytest_plugins = ["pytester"]
 
 
-def test_juju_debug_log_on_failure(pytester: pytest.Pytester, tmp_path: pathlib.Path):
-    test_file1 = (pathlib.Path(__file__).parent / "dump_logs_tests.py").read_text()
+def test_juju_debug_log_on_fail(pytester: pytest.Pytester, tmp_path: pathlib.Path):
+    test_file1 = (pathlib.Path(__file__).parent / "dump_logs_tests_fail.py").read_text()
     test_file2 = test_file1.replace('get_juju("foo1")', 'get_juju("foo2")')
     test_file2 = test_file2.replace('get_juju("bar1")', 'get_juju("bar2")')
     pytester.makepyfile(test_file1=test_file1, test_file2=test_file2)  # type: ignore
@@ -102,6 +102,55 @@ def test_juju_debug_log_on_failure(pytester: pytest.Pytester, tmp_path: pathlib.
     bar2_full_log_lines = bar2_log_path.read_text().splitlines()
     assert bar2_full_log_lines
     assert not _in_line("Hello, it is I!", bar2_full_log_lines)
+
+
+def test_juju_debug_log_on_pass(pytester: pytest.Pytester, tmp_path: pathlib.Path):
+    test_file1 = (pathlib.Path(__file__).parent / "dump_logs_tests_pass.py").read_text()
+    test_file2 = test_file1.replace('get_juju("foo1")', 'get_juju("foo2")')
+    test_file2 = test_file2.replace('get_juju("bar1")', 'get_juju("bar2")')
+    pytester.makepyfile(test_file1=test_file1, test_file2=test_file2)  # type: ignore
+    custom_dir = tmp_path / "custom-logs"
+
+    result = pytester.runpytest_subprocess("--model", "model-t", "--dump-logs", str(custom_dir))
+
+    # We expect this session to pass.
+    outcomes = result.parseoutcomes()
+    print(outcomes)
+    assert outcomes.get("passed") == 2
+    assert "failed" not in outcomes
+
+    # Debug log should NOT be emitted to stderr when tests pass.
+    stdout = result.stdout.str()
+    assert "Logging last 1000 lines of `juju debug-log` for model" not in stdout
+    assert "--- end of `juju debug-log`" not in stdout
+
+    # The full logs are still written with --dump-logs even on success.
+    foo1_log_path = custom_dir / "model-t-foo1-juju-debug.log"
+    assert foo1_log_path.exists()
+    foo1_full_log_lines = foo1_log_path.read_text().splitlines()
+    assert len(foo1_full_log_lines) > 10_000  # log action logs 10k times
+    assert _in_line("Hello, it is I! '1'", foo1_full_log_lines)
+    assert _in_line("Hello, it is I! '10000'", foo1_full_log_lines)
+    bar1_log_path = custom_dir / "model-t-bar1-juju-debug.log"
+    assert bar1_log_path.exists()
+    bar1_full_log_lines = bar1_log_path.read_text().splitlines()
+    assert len(bar1_full_log_lines) > 10_000  # log action logs 10k times
+    assert _in_line("Hello, it is I! '1'", bar1_full_log_lines)
+    assert _in_line("Hello, it is I! '10000'", bar1_full_log_lines)
+
+    # test_file2
+    foo2_log_path = custom_dir / "model-t-foo2-juju-debug.log"
+    assert foo2_log_path.exists()
+    foo2_full_log_lines = foo2_log_path.read_text().splitlines()
+    assert len(foo2_full_log_lines) > 10_000  # log action logs 10k times
+    assert _in_line("Hello, it is I! '1'", foo2_full_log_lines)
+    assert _in_line("Hello, it is I! '10000'", foo2_full_log_lines)
+    bar2_log_path = custom_dir / "model-t-bar2-juju-debug.log"
+    assert bar2_log_path.exists()
+    bar2_full_log_lines = bar2_log_path.read_text().splitlines()
+    assert len(bar2_full_log_lines) > 10_000  # log action logs 10k times
+    assert _in_line("Hello, it is I! '1'", bar2_full_log_lines)
+    assert _in_line("Hello, it is I! '10000'", bar2_full_log_lines)
 
 
 def _index_contains(lines: Iterable[str], target: str) -> int:
