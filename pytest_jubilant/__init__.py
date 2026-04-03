f9e42d62e85fe4c341a1da728f1e7c1bf9e93567
David Wilding <david.wilding@canonical.com>
2026-04-03 11:18:38 +0800

    replace welcome docstring

--- pytest_jubilant/__init__.py
+++ pytest_jubilant/__init__.py
@@ -2,7 +2,10 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Welcome to pytest-jubilant!"""
+"""pytest-jubilant provides pytest-specific functionality on top of Jubilant.
+
+Jubilant is a Pythonic wrapper around the Juju CLI.
+"""
 
 from pytest_jubilant._main import JujuFactory
 
